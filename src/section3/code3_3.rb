43465510ea23729c66406b3b6aa13ecc19d8945b
masahito1997 <velonica.gale2227@gmail.com>
2021-02-02 15:24:35 +0900

    add comment

--- src/section3/code3_3.rb
+++ src/section3/code3_3.rb
@@ -1,3 +1,11 @@
+#
+# 学んだことをコメントを書くようにする
+#
+# - Fixnumの最大値と最小値は
+#   - max: 4611686018427387903
+#   - min: -4611686018427387904
+#   - ref: https://simanman.hatenablog.com/entry/2014/01/07/154357
+#
 n = gets.chomp.to_i
 array = (0...n).map { gets.chomp.to_i }
 
